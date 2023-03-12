dd931ae932e23396d7dc785c65c5d8d9702dc827
Wayne_Mac <wayne65@gmail.com>
2023-03-13 00:00:51 +0800

    feat: Add 'ping' to show latency

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -2,5 +2,10 @@ module.exports = {
 	name: 'ping',
 	run(message, args) {
 		message.channel.send({ content: 'Pong.' });
+		message.reply(
+			`Latency is ${
+				Date.now() - message.createdTimestamp
+			}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`
+		);		
 	},
 };
