69e058e50c443de550714f41d9fe81f59268e643
Wayne_Mac <wayne65@gmail.com>
2023-03-20 00:53:22 +0800

    feat: Add gpt35ResponseSimple

--- commands/openai/ai2.js
+++ commands/openai/ai2.js
@@ -7,7 +7,7 @@ const ERROR_MSG_DESCRIPT = '\n格式為：ai <想問的問題> \n例如：ai 妳
 const ERROR_MSG_FORMAT = ERROR_MSG_FORMAT_TITLE + ERROR_MSG_DESCRIPT;
 
 module.exports = {
-	name: 'ai',
+	name: 'ai2',
 	async run(message, args) {
         const ask = message.content.slice(3);
         if (!ask) {
@@ -15,7 +15,7 @@ module.exports = {
             message.reply(ERROR_MSG_FORMAT);
             return;
         }
-        const resp = await openai.response(ask, message, 'gpt3');
+        const resp = await openai.response(ask, message, 'gpt35');
         fmlog('sys_msg', [String(message.author.username) + '-' + ask, resp]);
         message.reply(resp);
 	},
